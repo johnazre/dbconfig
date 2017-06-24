b48fb92ee1e064191488b5f04da5228eb0e00258
John <jaboyak@gmail.com>
2017-06-23 19:38:33 -0700

    testing

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('User module', () => {  
+  describe('"up"', () => {
+    it('should export a function', () => {
+      expect(true).to.equal(true)
+    })
+  })
+})
