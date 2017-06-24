b3f5b2a0ee689073a04a4f675c97e2057ca037f6
John <jaboyak@gmail.com>
2017-06-23 20:32:55 -0700

    added some testing

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,23 @@
 'use strict'
 
-const expect = require('chai').expect
+const expect = require('chai').expect;
+let findFlagValue = require('../lib/util_files').findFlagValue;
 
-describe('User module', () => {  
-  describe('"up"', () => {
-    it('should export a function', () => {
-      expect(true).to.equal(true)
+describe('util_files', () => {
+    it('should export a function called findFlagValue', () => {
+        expect(findFlagValue).to.be.a('function')
     })
-  })
+})
+
+describe('findFlagValue', () => {
+    it('should export a function', () => {
+        var flag = findFlagValue([ 
+            '/usr/local/bin/node', 
+            '/Users/someone/Desktop/dbconfig/dbconfig.js',
+            'knex', 
+            'scaffold',
+            '--create-dir=hello' 
+        ], '--create-dir');
+        expect(flag).to.equal('hello');
+    })
 })
