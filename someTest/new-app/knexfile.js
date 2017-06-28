b790be2c83a616615208bbb298c42cbb65feb65c
John <jaboyak@gmail.com>
2017-06-27 19:47:18 -0700

    updated routes flag

---
+++ someTest/new-app/knexfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+      client: 'pg',
+      connection: 'postgres://localhost/knex_todos',
+      migrations: {
+          directory: __dirname + '/db/migrations',
+        },
+      seeds: {
+          directory: __dirname + '/db/seeds',
+        },
+    },
+  production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+          directory: __dirname + '/db/migrations',
+        },
+      seeds: {
+          directory: __dirname + '/db/seeds/production',
+        },
+    },
+};
