4211cf98f0925b6b8bf07bc9d134170b4713ecd4
John Armbruster <jaboyak@gmail.com>
2017-12-19 19:40:22 -0700

    updated stuff

--- dbconfig.js
+++ dbconfig.js
@@ -5,24 +5,23 @@ const fs = require('fs');
 const inquirer = require('inquirer')
 const questions = require('./lib/questions')
 
-const knex_files = require('./lib/scaffolds/knexjs.scaffold');
-const obj_files = require('./lib/scaffolds/objectionjs.scaffold');
+// const knex_files = require('./lib/scaffolds/knexjs.scaffold');
+// const obj_files = require('./lib/scaffolds/objectionjs.scaffold');
+//
+// const shared_files = require('./lib/scaffolds/shared.scaffold');
+// const help_data = require('./lib/help_blurb');
+// const util_files = require('./lib/util_files');
+const builders = require('./lib/builders')
 
-const shared_files = require('./lib/scaffolds/shared.scaffold');
-const help_data = require('./lib/help_blurb');
-const util_files = require('./lib/util_files');
-const flag_functions = require('./lib/flag_functions');
-
-const directoryName = flag_functions.findFlagValue(process.argv, '--create-dir') || 'new-app';
-const router = flag_functions.findFlag(process.argv, '--router');
-
-inquirer.prompt(questions).then(answers => {
-  console.log(JSON.stringify(answers, null, '  '));
-});
-
-// if(!process.argv[2] || process.argv.includes('--help')) {
-//   console.log("\x1b[36m", help_data.help);
-// } else {
+if(process.argv.includes('--help')) {
+  console.log("\x1b[36m", help_data.help);
+} else {
+  inquirer.prompt(questions).then(answers => {
+    exports.answers = answers
+    builders()
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+}
 //   if(process.argv[2].toLowerCase() === 'knex') {
 //     if(process.argv[3].toLowerCase() === 'scaffold') {
 //       util_files.generateScaffold(router);
