e80feb6be35d34d63d1488384859d0f2ddc86292
John <jaboyak@gmail.com>
2018-10-01 10:55:55 -0700

    testing some conditional questions

--- dbconfig.js
+++ dbconfig.js
@@ -4,15 +4,18 @@
 // Questions packages and files
 const fs = require('fs')
 const inquirer = require('inquirer')
-const questions = require('./lib/questions')
+const initQuestions = require('./lib/questions/init')
+const fileGenQuestions  = require('./lib/questions/file-gen')
 
 const help_data = require('./lib/help_blurb')
 const builders = require('./lib/builders')
 
 if (process.argv.includes('--help')) {
   console.log('\x1b[36m', help_data.help)
-} else {
-  inquirer.prompt(questions).then(answers => {
+} else if(process.argv.includes('init')) {
+  inquirer.prompt(initQuestions).then(answers => {
     builders(answers)
   })
+} else if(process.argv.includes('new')) {
+  require('./lib/questions/file-gen')
 }
